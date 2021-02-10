78f0325b00b9bed1102517fb708cb9dc5ba7d862
Zaftan <brynn.rothuis@hotmail.com>
2021-02-10 17:05:05 +0100

    Added background
    To do collisions and adding better bullet despawn + backgrounmd overlay

---
+++ Game/GXPEngine/GXPEngine/Background.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GXPEngine;
+
+class Background : AnimationSprite
+{
+    public Background() : base(Settings.ASSET_PATH + "Art/Road.gif", 1, 1, addCollider: false)
+    {
+        SetOrigin(-300, -80);
+    }
+}
